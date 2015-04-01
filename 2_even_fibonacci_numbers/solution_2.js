6bb1e4bdb3a7eaf7940a1409ebb7a5101a26af92
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-03-31 23:06:15 -0400

    fix a bug that could have been a huge issue

--- 2_even_fibonacci_numbers/solution_2.js
+++ 2_even_fibonacci_numbers/solution_2.js
@@ -8,19 +8,30 @@ var i = 2;
 // Int to store the sum
 var sum = 0;
 
-// Loop until
+// Loop until last number is larger than our limit
 while (fibonacciSequence[i] < 4000000) {
+	
+	// Add the sum of our last two numbers together to create the next
 	fibonacciSequence.push(fibonacciSequence[i] + fibonacciSequence[(i - 1)]);
+	
+	// Increase the array index tracker
 	i++;
 }
 
-for (j = 0; j < fibonacciSequence.length; j++) {
-	//if (fibonacciSequence[j] < 4000000) {
-		if (fibonacciSequence[j] % 2 === 0) {
-				sum += fibonacciSequence[j];
-			}
-	//}
+// Remove the last entry from our sequence since it MUST
+// be greater than 4000000 to exit the loop
+fibonacciSequence.pop();
 
+// Loop through all our fibonacci numbers
+for (j = 0; j < fibonacciSequence.length; j++) {
+
+	// Check if its divisible by 2
+	if (fibonacciSequence[j] % 2 === 0) {
+		
+		// Add it to the running total
+		sum += fibonacciSequence[j];
+	}
 }
 
+// Lets see our total!
 console.log(sum);
